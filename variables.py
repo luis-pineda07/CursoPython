e5528be5e49bcee7af8b318ab2cad5b72d58c00a
Josue07 <luis_07-299@hotmail.com>
2024-03-17 14:55:35 -0500

    Variables

---
+++ variables.py
@@ -0,0 +1,25 @@
+#Variables
+x = 10
+y = 5.5
+z = x + y
+
+print ("el valor de z es : " + str(z))
+
+#variables de cadena
+nombre= "Jose"
+apellido= "Fernandez"
+
+nombre_completo = nombre + " " + apellido
+print("El nombre es: " + nombre_completo)
+
+#variables logicas
+es_verdadero = True
+es_falso = not es_verdadero
+
+print(es_falso)
+
+#secuencia
+mi_list = [1, 2, 3, 4, 5]
+mi_tupa = (6, 7, 8, 9, 10)
+
+print (mi_tupa)
