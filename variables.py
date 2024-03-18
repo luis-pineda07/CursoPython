eea5e95374544b1bb292ce3be9a52ea4aa46671b
Josue07 <luis_07-299@hotmail.com>
2024-03-17 19:11:28 -0500

    Variables de python

--- variables.py
+++ variables.py
@@ -20,6 +20,19 @@ print(es_falso)
 
 #secuencia
 mi_list = [1, 2, 3, 4, 5]
-mi_tupa = (6, 7, 8, 9, 10)
+mi_tupla = (6, 7, 8, 9, 10)
 
-print (mi_tupa)
+print(mi_tupla)
+print("Mi primer elemento " + str(mi_tupla[0]))
+print("Mi ultimo elemento " + str(mi_tupla[len(mi_tupla) - 1]))
+
+#dicciionario
+mi_diccionario = {
+    "nombre": "Jose",
+    "apellido": "Fernandez",
+    "nationality": "Suecia",
+    "age": 26,
+
+}    
+print(mi_diccionario) 
+print("El nombre es: " + mi_diccionario["nombre"])
